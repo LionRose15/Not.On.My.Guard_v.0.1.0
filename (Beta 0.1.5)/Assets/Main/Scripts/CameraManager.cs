71d449048359ae9a84029ed9f9874dbce59d23ce
LionRose15 <zap562@amerike.edu.mx>
2022-11-07 08:16:13 -0600

    Changes in the names of the variables of the scripts

--- (Beta 0.1.5)/Assets/Main/Scripts/CameraManager.cs
+++ (Beta 0.1.5)/Assets/Main/Scripts/CameraManager.cs
@@ -4,25 +4,25 @@ namespace Basics
 {
     public class CameraManager : MonoBehaviour
     {
-        [SerializeField] private float Sensitivity = 0.5f;
+        [SerializeField] private float _sensitivity = 0.5f;
 
-        private float _NewRotation = default;
-        private Vector3 _LastRotation = default;
+        private float _newRotation = default;
+        private Vector3 _lastRotation = default;
 
         void Update()
         {
             if (Input.GetMouseButtonDown(0))
             {
-                _NewRotation = Input.mousePosition.x;
-                _LastRotation = transform.eulerAngles;
+                _newRotation = Input.mousePosition.x;
+                _lastRotation = transform.eulerAngles;
             }
 
             else if (Input.GetMouseButton(0))
             {
-                var difference = Input.mousePosition.x - _NewRotation;
-                var newRotation = _LastRotation;
+                var difference = Input.mousePosition.x - _newRotation;
+                var newRotation = _lastRotation;
 
-                newRotation.y = _LastRotation.y + (difference * Sensitivity);
+                newRotation.y = _lastRotation.y + (difference * _sensitivity);
                 transform.eulerAngles = newRotation;
             }
         }
